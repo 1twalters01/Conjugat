73cdde2efa0ddaa82be9f06e02b108a041197bed
1twalters01 <1twalters01@gmail.com>
2023-02-13 13:31:58 +0000

    password reset page 2 design finished

--- conjugat/account/urls.py
+++ conjugat/account/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('password-reset/', views.NewPasswordResetView.as_view(), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password-reset/<uidb64>/<token>/', views.NewPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('password-resetcomplete//', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('register/', views.register, name='register'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]
