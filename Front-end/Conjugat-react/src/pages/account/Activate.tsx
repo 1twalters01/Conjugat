afdf0015a1ca96556ab90e8bf2035130303705eb
1twalters01 <1twalters01@gmail.com>
2023-02-21 02:11:58 +0000

    register

--- Front-end/Conjugat-react/src/pages/account/Activate.tsx
+++ Front-end/Conjugat-react/src/pages/account/Activate.tsx
@@ -3,17 +3,23 @@ import Axios from 'axios'
 import Authorization from '../../Authorization'
 
 function Activate() {
+  Authorization.NotAuthRequired()
+  
+  activateAccount()
+
+  return (
+    <h1>Activate</h1>
+  )
+}
+
+function activateAccount() {
   const { uidb64, token } = useParams();
+  console.log(token)
   const url = "http://conjugat.io:8000/account/activate/"
   Axios.post(url, {
     uidb64: uidb64,
     token: token,
   })
-
-  Authorization.NotAuthRequired()
-  return (
-    <h1>Activate</h1>
-  )
 }
 
 export default Activate
