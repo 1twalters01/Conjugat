f30c73dfa1239fc5109fb54e9bbc4f0ac280e978
1twalters01 <1twalters01@gmail.com>
2023-04-03 10:55:33 +0100

    remove design folder as I used it for the front end

--- Designs/Prototype html/Home/Landing Page/sizing.js
+++
@@ -1,8 +0,0 @@
-let main_container = document.querySelector('.main-container');
-let background = document.querySelector('.background');
-
-background.style.height = main_container.offsetHeight + 'px';
-addEventListener("scroll", (event) => {
-    background.style.height = main_container.offsetHeight + 'px'
-}
-);
