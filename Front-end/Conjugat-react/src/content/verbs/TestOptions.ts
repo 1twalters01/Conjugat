9a00e3333636548417bc549e9e0005618f867e67
1twalters01 <1twalters01@gmail.com>
2023-05-16 22:54:44 +0100

    test Metadata

--- Front-end/Conjugat-react/src/content/verbs/TestOptions.ts
+++ Front-end/Conjugat-react/src/content/verbs/TestOptions.ts
@@ -1,22 +1,22 @@
 export const translations = {
     'English': {
         'Title1': 'Duolingo Test settings',
-        'metaContent1': '',
+        'metaContent1': 'Duolingo Test settings',
     },
     'French': {
         'Title1': 'Paramètres du test Duolingo',
-        'metaContent1': '',
+        'metaContent1': 'Paramètres du test Duolingo',
     },
     'Italian': {
         'Title1': 'Impostazioni del test di Duolingo',
-        'metaContent1': '',
+        'metaContent1': 'Impostazioni del test di Duolingo',
     },
     'Portuguese': {
         'Title1': 'Definições do teste Duolingo',
-        'metaContent1': '',
+        'metaContent1': 'Definições do teste Duolingo',
     },
     'Spanish': {
         'Title1': 'Configuración de Duolingo Test',
-        'metaContent1': '',
+        'metaContent1': 'Configuración de Duolingo Test',
     }
 }
--- Front-end/Conjugat-react/src/content/verbs/TestResults.ts
+++ Front-end/Conjugat-react/src/content/verbs/TestResults.ts
@@ -1,22 +1,22 @@
 export const translations = {
     'English': {
         'Title1': 'Test results',
-        'metaContent1': '',
+        'metaContent1': 'Conjguat test results',
     },
     'French': {
         'Title1': 'Résultats des tests',
-        'metaContent1': '',
+        'metaContent1': 'Résultats du test Conjugat',
     },
     'Italian': {
         'Title1': 'Risultati del test',
-        'metaContent1': '',
+        'metaContent1': 'Risultati del test Conjugat',
     },
     'Portuguese': {
         'Title1': 'Resultados dos testes',
-        'metaContent1': '',
+        'metaContent1': 'Resultados do teste Conjugat',
     },
     'Spanish': {
         'Title1': 'Resultados de las pruebas',
-        'metaContent1': '',
+        'metaContent1': 'Resultados de la prueba Conjugat',
     }
 }
