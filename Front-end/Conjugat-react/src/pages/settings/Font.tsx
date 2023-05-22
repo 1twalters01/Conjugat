4735b69ccde511c8027bbffe5ef64d8c800614c7
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2023-05-22 10:40:26 +0100

    Update Font.tsx

--- Front-end/Conjugat-react/src/pages/settings/Font.tsx
+++ Front-end/Conjugat-react/src/pages/settings/Font.tsx
@@ -8,21 +8,44 @@ import { translations } from "../../content/settings/Font"
 function Font() {
     Authorization.AuthRequired()
     const { language } = useSelector((state: RootState) => state.persistedReducer.language)
-    return (
-        <>
-        <Helmet>
-            <title>{getTranslation(translations, language, 'Title1')}</title>
-            <meta name="description"
-                content={getTranslation(translations, language, 'metaContent1')}
-            />
-            <link rel="canonical" href="/settings/premium" />
-        </Helmet>
+ 	const [loaded, isLoaded] = useState(true)
+	const [done, isDone] = useState(false)
+	if (done === false) {
+		return (
+	        <>
+        		<Helmet>
+            		<title>{getTranslation(translations, language, 'Title1')}</title>
+            		<meta name="description"
+            			content={getTranslation(translations, language, 'metaContent1')}
+            		/>
+            		<link rel="canonical" href="/settings/font" />
+        		</Helmet>
 
-        <div>
+        		<div>
             
-        </div>
-        </>
-    )
+        		</div>
+        	</>
+	    )
+	else if (done === true) {
+		return (
+			<>
+				<Helmet>
+					<title>{getTranslation(translations, language, 'Title2')}</title>
+            		<meta name="description"
+            			content={getTranslation(translations, language, 'metaContent2')}
+            		/>
+            		<link rel="canonical" href="/settings/font" />
+				</Helmet>
+
+				<div>
+				
+				</div>
+			</>
+		)
+	}
+	else {
+		return (<></>)
+	}
 }
 
 export default Font
