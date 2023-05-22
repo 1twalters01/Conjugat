28837af83f4afba34d4c4b7ad8983b5e4debf8fe
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2023-05-22 13:33:01 +0100

    Update Language.tsx

--- Front-end/Conjugat-react/src/pages/settings/Language.tsx
+++ Front-end/Conjugat-react/src/pages/settings/Language.tsx
@@ -8,21 +8,55 @@ import { translations } from "../../content/settings/Language"
 function Language() {
     Authorization.AuthRequired()
     const { language } = useSelector((state: RootState) => state.persistedReducer.language)
-    return (
-        <>
-        <Helmet>
-            <title>{getTranslation(translations, language, 'Title1')}</title>
-            <meta name="description"
-                content={getTranslation(translations, language, 'metaContent1')}
-            />
-            <link rel="canonical" href="/settings/premium" />
-        </Helmet>
+	const [done, setDone] = useState(false)
+	if (done === false) {
+		return (
+	        <>
+        		<Helmet>
+            		<title>{getTranslation(translations, language, 'Title1')}</title>
+            		<meta name="description"
+            			content={getTranslation(translations, language, 'metaContent1')}
+            		/>
+            		<link rel="canonical" href="/settings/language" />
+        		</Helmet>
 
-        <div>
-            
-        </div>
-        </>
-    )
+        		<div className="rhs container">
+					<div className="Header-spacer">
+						<h1 className="text">{getTranslation(translations, language, 'Text1'</h1
+					</div>
+					
+					<div className="form-spacer">
+						<LanguageForm language={language} onDoneChange={setDone} />
+					</div
+        		</div>
+        	</>
+	    )
+	else if (done === true) {
+		return (
+			<>
+				<Helmet>
+					<title>{getTranslation(translations, language, 'Title2')}</title>
+            		<meta name="description"
+            			content={getTranslation(translations, language, 'metaContent2')}
+            		/>
+            		<link rel="canonical" href="/settings/font" />
+				</Helmet>
+
+				<div className="rhs container">
+					<div className="Header-spacer">
+						<h1 className="text">{getTranslation(translations, language, 'Text2')}</h1>
+					</div>
+
+					<div className="para">
+						<p className="text">{getTranslation(translations, language, 'Text3')}</p>
+					</div>
+				</div>
+			</>
+		)
+	}
+	else {
+		return (<></>)
+	}
 }
 
 export default Language
